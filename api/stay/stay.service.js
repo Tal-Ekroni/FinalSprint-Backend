66403d94eb8539cf9afddf890dde408310f24a5b
Tal EkronI <talekroni01@gmail.com>
2021-10-03 02:15:23 +0300

    first commit

---
+++ api/stay/stay.service.js
@@ -0,0 +1,81 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const ObjectId = require('mongodb').ObjectId
+
+
+async function query(filterBy) {
+    try {
+
+        // const criteria = _buildCriteria(filterBy)
+        const collection = await dbService.getCollection('stay')
+        var stays = await collection.find().toArray()
+        return stays
+    } catch (err) {
+        console.log(err);
+        logger.error('cannot find stays', err)
+        throw err
+    }
+}
+
+function _buildCriteria(filterBy) {
+    const criteria = {}
+    if (filterBy.name) {
+        criteria.name = { $regex: filterBy.name, $options: 'i' }
+    }
+    return criteria
+}
+
+
+async function getById(stayId) {
+    try {
+        const collection = await dbService.getCollection('stay')
+        const stay = collection.findOne({ '_id': ObjectId(stayId) })
+        return stay
+    } catch (err) {
+        logger.error(`while finding stay ${stayId}`, err)
+        throw err
+    }
+}
+
+async function remove(stayId) {
+    try {
+        const collection = await dbService.getCollection('stay')
+        await collection.deleteOne({ '_id': ObjectId(stayId) })
+        return stayId
+    } catch (err) {
+        logger.error(`cannot remove stay ${stayId}`, err)
+        throw err
+    }
+}
+
+async function add(stay) {
+    try {
+        console.log(stay);
+        const collection = await dbService.getCollection('stay')
+        const addedStay = await collection.insertOne(stay)
+        return addedStay
+    } catch (err) {
+        logger.error('cannot insert stay', err)
+        throw err
+    }
+}
+async function update(stay) {
+    try {
+        var id = ObjectId(stay._id)
+        delete stay._id
+        const collection = await dbService.getCollection('stay')
+        await collection.updateOne({ "_id": id }, { $set: { ...stay } })
+        return stay
+    } catch (err) {
+        logger.error(`cannot update stay ${stayId}`, err)
+        throw err
+    }
+}
+
+module.exports = {
+    remove,
+    query,
+    getById,
+    add,
+    update,
+}
