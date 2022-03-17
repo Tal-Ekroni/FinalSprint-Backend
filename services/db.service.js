88f245e54acc53a05d5e204c5a8bdfaf6ca44de1
Tal Ekroni <talekroni01@gmail.com>
2022-03-17 13:37:13 +0200

    database update

--- services/db.service.js
+++ services/db.service.js
@@ -7,95 +7,95 @@ module.exports = {
 };
 
 // Database Name
-const dbName = "AnyGo";
+const dbName = "anyGoDB";
 var dbConn = null;
-const dbNameCopy = "anyGoDB";
-var dbConnCopy = null;
-getData();
-async function getData() {
-  try {
-    const staysCol = await getCollection("stay");
-    const stays = await staysCol.find({}).toArray();
+// const dbNameCopy = "AnyGo";
+// var dbConnCopy = null;
+// getData();
+// async function getData() {
+//   try {
+//     const staysCol = await getCollection("stay");
+//     const stays = await staysCol.find({}).toArray();
 
-    const usersCol = await getCollection("user");
-    const users = await usersCol.find({}).toArray();
+//     const usersCol = await getCollection("user");
+//     const users = await usersCol.find({}).toArray();
 
-    const ordersCol = await getCollection("order");
-    const orders = await ordersCol.find({}).toArray();
+//     const ordersCol = await getCollection("order");
+//     const orders = await ordersCol.find({}).toArray();
 
-    const usersa = users.map((u) => {
-      const res = {};
-      Object.keys(u)
-        .filter((k) => k !== "_id")
-        .forEach((k) => {
-          res[k] = u[k];
-        });
-      return res;
-    });
+//     const usersa = users.map((u) => {
+//       const res = {};
+//       Object.keys(u)
+//         .filter((k) => k !== "_id")
+//         .forEach((k) => {
+//           res[k] = u[k];
+//         });
+//       return res;
+//     });
 
-    const ordersa = orders.map((u) => {
-      const res = {};
-      Object.keys(u)
-        .filter((k) => k !== "_id")
-        .forEach((k) => {
-          res[k] = u[k];
-        });
-      return res;
-    });
-    const staysa = stays.map((u) => {
-      const res = {};
-      Object.keys(u)
-        .filter((k) => k !== "_id")
-        .forEach((k) => {
-          res[k] = u[k];
-        });
-      return res;
-    });
-    console.log('ordersa,staysa,usersa');
-    const collectionU = await getCollectionCopy("user");
-    const collectionS = await getCollectionCopy("stay");
-    const collectionO = await getCollectionCopy("order");
+//     const ordersa = orders.map((u) => {
+//       const res = {};
+//       Object.keys(u)
+//         .filter((k) => k !== "_id")
+//         .forEach((k) => {
+//           res[k] = u[k];
+//         });
+//       return res;
+//     });
+//     const staysa = stays.map((u) => {
+//       const res = {};
+//       Object.keys(u)
+//         .filter((k) => k !== "_id")
+//         .forEach((k) => {
+//           res[k] = u[k];
+//         });
+//       return res;
+//     });
+//     console.log('ordersa,staysa,usersa');
+//     const collectionU = await getCollectionCopy("user");
+//     const collectionS = await getCollectionCopy("stay");
+//     const collectionO = await getCollectionCopy("order");
 
-    console.log('collections, ', collectionU, collectionS, collectionO);
+//     console.log('collections, ', collectionU, collectionS, collectionO);
 
-    const addedU = await collectionU.insertMany(usersa);
-    const addedS = await collectionS.insertMany(staysa);
-    const addedO = await collectionO.insertMany(ordersa);
-    console.log("hyo");
-  } catch (err) {
-    throw err;
-  }
-}
+//     const addedU = await collectionU.insertMany(usersa);
+//     const addedS = await collectionS.insertMany(staysa);
+//     const addedO = await collectionO.insertMany(ordersa);
+//     console.log("hyo");
+//   } catch (err) {
+//     throw err;
+//   }
+// }
 
 
-async function getCollectionCopy(collectionName) {
-  try {
-    const db = await connectCopy();
-    const collection = await db.collection(collectionName);
-    console.log("coll", collection);
-    return collection;
-  } catch (err) {
-    throw err;
-  }
-}
+// async function getCollectionCopy(collectionName) {
+//   try {
+//     const db = await connectCopy();
+//     const collection = await db.collection(collectionName);
+//     console.log("coll", collection);
+//     return collection;
+//   } catch (err) {
+//     throw err;
+//   }
+// }
 
-async function connectCopy() {
-  if (dbConnCopy) return dbConnCopy;
-  try {
-    const client = await MongoClient.connect(
-      "mongodb+srv://talek:Tal18Javadrift@cluster0.vhtil.mongodb.net/anyGoDB?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    const db = client.db(dbNameCopy);
-    dbConn = db;
-    return db;
-  } catch (err) {
-    throw err;
-  }
-}
+// async function connectCopy() {
+//   if (dbConnCopy) return dbConnCopy;
+//   try {
+//     const client = await MongoClient.connect(
+//       "mongodb+srv://talek:Tal18Javadrift@cluster0.vhtil.mongodb.net/anyGoDB?retryWrites=true&w=majority",
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       }
+//     );
+//     const db = client.db(dbNameCopy);
+//     dbConn = db;
+//     return db;
+//   } catch (err) {
+//     throw err;
+//   }
+// }
 
 async function getCollection(collectionName) {
   try {
